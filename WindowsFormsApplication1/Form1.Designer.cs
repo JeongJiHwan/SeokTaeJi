5baa3dd668f488e5395b90a5e1c42f285828ad59
JeongJiHwan <wlghks0461@gmail.com>
2020-07-07 16:51:51 +0900

    Add Files

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(14, 88);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(777, 800);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 915);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "카메라 On";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(441, 915);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 68);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "거울모드 On";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(560, 915);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 68);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "알람 Off";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(679, 915);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 68);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "측정시작";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(74, 934);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "기준온도: ";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(798, 88);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(732, 799);
+            this.listBox1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(182, 928);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(43, 34);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gulim", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(232, 934);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "℃";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(27, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "현재 시각: ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBox2.Location = new System.Drawing.Point(128, 48);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(392, 27);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBox3.Location = new System.Drawing.Point(798, 46);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(383, 27);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(1363, 934);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "검색:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "전체",
+            "정상",
+            "경고"});
+            this.comboBox1.Location = new System.Drawing.Point(1429, 928);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(77, 37);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.Text = "전체";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(1446, 989);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 17);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "석태지   V 1.0.0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1554, 1016);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "석태지 열화상 프로그램";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System.Threading;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture capture;
+        Mat frame;
+        Bitmap image;
+        private Thread camera;
+        int isCameraRunning = 0;
+        Font arialFont = new Font("Arial", 20);
+        Rect[] faces;
+        float[] val = new float[10];
+        DataMgr dMgr = new DataMgr();
+        float standard = 70;
+        private void CaptureCamera()
+        {
+
+            camera = new Thread(new ThreadStart(CaptureCameraCallback));
+            camera.Start();
+        }
+
+        private void CaptureCameraCallback()
+        {
+            frame = new Mat();
+            capture = new VideoCapture();
+            capture.FrameWidth = 680;
+            capture.FrameHeight = 480;
+            capture.Open(0);
+            String filenameFaceCascade = "haarcascade_frontalface_alt.xml";
+            CascadeClassifier faceCascade = new CascadeClassifier();
+
+            if (!faceCascade.Load(filenameFaceCascade))
+            {
+                MessageBox.Show("error");
+                return;
+            }
+            while (isCameraRunning == 1)
+            {
+                capture.Read(frame);
+                if (!frame.Empty())
+                {
+                    faces = faceCascade.DetectMultiScale(frame);
+                    if (faces.Length > 0)
+                    {
+                        for (int i = 0; i < faces.Length; i++)
+                        {
+                            Cv2.Rectangle(frame, faces[i], Scalar.Red); // add rectangle to the image
+                            val[i] = (float)Cv2.Mean(frame.SubMat(faces[i]));
+                            //textBox1.Text = textBox1.Text + "\tfaces : " + faces[i];
+                        }
+
+                        image = BitmapConverter.ToBitmap(frame);
+
+                        using (Graphics graphics = Graphics.FromImage(image))
+                        {
+                            for (int i = 0; i < faces.Length; i++)
+                            {
+                                PointF p = new PointF(faces[i].X + faces[i].Width / 2 - 10, faces[i].Y - 15);
+                                if (val[i] > standard)
+                                {
+                                    graphics.DrawString(val[i].ToString(), arialFont, Brushes.Red, p);
+                                }
+                                else
+                                {
+                                    graphics.DrawString(val[i].ToString(), arialFont, Brushes.Green, p);
+                                }
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        image = BitmapConverter.ToBitmap(frame);
+                    }
+                    pictureBox1.Image = image;
+                }
+                image = null;
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text.Equals("카메라 On"))
+            {
+                CaptureCamera();
+                button1.Text = "카메라 Off";
+                isCameraRunning = 1;
+            }
+            else
+            {
+                List<Data> dList = new List<Data>();
+                button1.Text = "카메라 On";
+                isCameraRunning = 0;
+
+                for (int i = 0; i < faces.Length; i++)
+                {                    
+                    try
+                    {
+                        Mat dst = frame.SubMat(faces[i]);
+                        DateTime time = DateTime.Now;
+                        string str = time.ToString("yyyyMMddhhmmss");
+
+                        string filename = string.Format("../../faces/{0}.jpg", str);
+
+                        //Cv2.ImShow(filename, dst);
+                        Cv2.ImWrite(filename, dst);
+
+                        Data d = new Data() { Date = time.ToString("yyyy/MM/dd"), Time = time.ToString("hh:mm:ss"), face = filename, measure = val[i].ToString(), stand = standard.ToString(), warn = val[i] > float.Parse("75") };
+                        dMgr.inputData(d);
+                        dList.Add(d);
+                        Thread.Sleep(1000);
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("SubMat Error");
+                    }
+                }
+
+                if (capture.IsOpened())
+                {
+                    capture.Release();
+                }
+
+                dMgr.DisplayData(listBox1, dList);
+            }
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text.Equals("거울모드 On"))
+            {
+                CaptureCamera();
+                button2.Text = "거울모드 Off";
+                isCameraRunning = 1;
+            }
+            else
+            {
+                if (capture.IsOpened())
+                {
+                    capture.Release();
+                }
+
+                button2.Text = "거울모드 On";
+                isCameraRunning = 0;
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text.Equals("알람 Off"))
+            {
+                CaptureCamera();
+                button3.Text = "알람 On";
+                isCameraRunning = 1;
+            }
+            else
+            {
+                if (capture.IsOpened())
+                {
+                    capture.Release();
+                }
+
+                button3.Text = "알람 Off";
+                isCameraRunning = 0;
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text.Equals("측정시작"))
+            {
+                CaptureCamera();
+                button4.Text = "카메라 Off";
+                isCameraRunning = 1;
+            }
+            else
+            {
+                if (capture.IsOpened())
+                {
+                    capture.Release();
+                }
+
+                button4.Text = "측정시작";
+                isCameraRunning = 0;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dMgr.LoadFromFile();
+            dMgr.DisplayData(listBox1);
+            textBox2.Text = DateTime.Now.ToString();
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            textBox2.Text = DateTime.Now.ToString();
+        }
+
+        struct Data
+        {
+            public override string ToString()
+            {
+                return string.Format("{0}\t{1}\t{2}\t{3}\t{4}", Date, Time, face, measure, stand, warn);
+            }
+            public void SaveToFile(StreamWriter sw)
+            {
+                sw.WriteLine(Date);
+                sw.WriteLine(Time);
+                sw.WriteLine(face);
+                sw.WriteLine(measure);
+                sw.WriteLine(stand);
+                sw.WriteLine(warn==true?"경고":"정상");
+            }
+            public void LoadFromFile(StreamReader sr)
+            {
+                Date = sr.ReadLine();
+                Time = sr.ReadLine();
+                face = sr.ReadLine();
+                measure = sr.ReadLine();
+                stand = sr.ReadLine();
+                warn = sr.ReadLine() == "정상" ? false : true;
+            }
+            public string Date;
+            public string Time;
+            public string face;
+            public string measure;
+            public string stand;
+            public bool warn;
+        };
+        class DataMgr
+        {
+            public void SaveToFile()
+            {
+                StreamWriter sw = new StreamWriter("../../data.txt");
+                sw.WriteLine("{0}", dList.Count);
+                foreach (Data m in dList)
+                {
+                    m.SaveToFile(sw);
+                }
+                sw.Close();
+                sw.Dispose();
+            }
+            public void LoadFromFile()
+            {
+                StreamReader sr = new StreamReader("../../data.txt");
+                int iCount = int.Parse(sr.ReadLine());
+                for (int i = 0; i < iCount; i++)
+                {
+                    Data m = new Data();
+                    m.LoadFromFile(sr);
+                    dList.Add(m);
+                }
+                sr.Close();
+                sr.Dispose();
+            }
+            public int DisplayData(ListBox list)
+            {
+                int cnt = 0;
+                list.Items.Clear();
+                for(int i=0; i<dList.Count; i++)
+                {
+                    list.Items.Add(dList[i]);
+                    if (dList[i].warn == true) cnt++;
+                }
+                return cnt;
+            }
+            public void DisplayData(ListBox list, List<Data> dataList)
+            {
+                for (int i = 0; i < dataList.Count; i++)
+                {
+                    list.Items.Add(dataList[i]);
+                }
+            }
+            public void inputData(Data d)
+            {
+                dList.Add(d);
+            }
+            List<Data> dList = new List<Data>();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            dMgr.SaveToFile();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            standard = float.Parse(textBox1.Text);
+        }
+    }
+    
+    
+}
